940e061f2429cc67827a9b51132bce6f406cd63c
99002482 <nisarga.hs@ltts.com>
2020-11-07 16:44:07 +0530

    "Adding Result Activity which display results such as correct answer,wrong answer and final results"

--- app/src/main/java/com/example/nisganbini/quizbee/ResultActivity.java
+++ app/src/main/java/com/example/nisganbini/quizbee/ResultActivity.java
@@ -1,14 +1,56 @@
 package com.example.nisganbini.quizbee;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ResultActivity extends AppCompatActivity {
+    TextView tv, tv2, tv3;
+    Button btnRestart,video;
+    Button explore;
+    public  static String TAG = QuestionActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        tv = (TextView)findViewById(R.id.tvres);
+        tv2 = (TextView)findViewById(R.id.tvres2);
+        tv3 = (TextView)findViewById(R.id.tvres3);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+        explore=(Button)findViewById(R.id.explore);
+        video=(Button)findViewById(R.id.video);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Correct answers: " + QuestionActivity.correct + "\n");
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong Answers: " + QuestionActivity.wrong + "\n");
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score: " + QuestionActivity.correct + "\n");
+        tv.setText(sb);
+        tv2.setText(sb2);
+        tv3.setText(sb3);
+
+        QuestionActivity.correct=0;
+        QuestionActivity.wrong=0;
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w(TAG,"Restart Button Clicked");
+                Intent in = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+            }
+        });
+
+
+
     }
+
 }
